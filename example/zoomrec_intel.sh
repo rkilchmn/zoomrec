836745f84516bd969706ab132619ddfb9abbf096
Roger Kilchenmann <roger@kilchenmann.net>
2023-02-06 09:22:21 +1100

    update starter scrips for samba

--- example/zoomrec_intel.sh
+++ example/zoomrec_intel.sh
@@ -6,10 +6,14 @@ docker run -d --restart unless-stopped \
   -e DISPLAY_NAME="Monica" \
   -e FFMPEG_OUTPUT_PARAMS="-acodec aac -b:a 128k -vaapi_device /dev/dri/renderD128 -vf 'hwupload,scale_vaapi=format=nv12' -c:v h264_vaapi -qp 24" \
   -e LIBVA_DRIVER_NAME=i965 \
+  -e SAMBA_USER=testuser \
+  -e SAMBA_PASS=test123 \
   -v /home/roger/zoomrec/recordings:/home/zoomrec/recordings \
   -v /home/roger/zoomrec/audio:/home/zoomrec/audio \
   -v /home/roger/zoomrec/meetings.csv:/home/zoomrec/meetings.csv:ro \
   -p 5901:5901 \
+  -p 137-139:137-139 \
+  -p 445:445 \
   --security-opt seccomp:unconfined \
   --group-add="44" \
   --group-add="110" \
