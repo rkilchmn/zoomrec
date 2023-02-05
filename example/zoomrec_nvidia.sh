d64b35d60af563adf33e7b1ab8b5f9e5667ca477
Roger Kilchenmann <roger@kilchenmann.net>
2023-02-06 09:19:57 +1100

    updates example start scripts

--- example/zoomrec_nvidia.sh
+++ example/zoomrec_nvidia.sh
@@ -6,10 +6,14 @@ docker run -d --restart unless-stopped \
   -e DISPLAY_NAME="Monica" \
   -e FFMPEG_INPUT_PARAMS="-hwaccel cuvid" \
   -e FFMPEG_OUTPUT_PARAMS="-c:v hevc_nvenc -gpu 0 -preset slow -acodec aac -b:a 128k" \
+  -e SAMBA_USER=testuser \
+  -e SAMBA_PASS=test123 \
   -v /home/roger/zoomrec/recordings:/home/zoomrec/recordings \
   -v /home/roger/zoomrec/audio:/home/zoomrec/audio \
   -v /home/roger/zoomrec/meetings.csv:/home/zoomrec/meetings.csv:ro \
   -p 5901:5901 \
+  -p 137-139:137-139 \
+  -p 445:445 \
   --security-opt seccomp:unconfined \
   --gpus all \
   rkilchmn/zoomrec:latest
