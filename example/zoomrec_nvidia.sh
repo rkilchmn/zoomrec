20241ba92ba3e983eb42a43d720998c075c206f7
Roger Kilchenmann <roger@kilchenmann.net>
2023-02-09 12:21:33 +1100

    added video bitrate

--- example/zoomrec_nvidia.sh
+++ example/zoomrec_nvidia.sh
@@ -5,9 +5,9 @@ docker run -d --restart unless-stopped \
   -e TZ=Australia/Sydney \
   -e DISPLAY_NAME="Monica" \
   -e FFMPEG_INPUT_PARAMS="-hwaccel cuvid" \
-  -e FFMPEG_OUTPUT_PARAMS="-c:v hevc_nvenc -gpu 0 -preset slow -acodec aac -b:a 128k" \
+  -e FFMPEG_OUTPUT_PARAMS="-c:v hevc_nvenc -b:v 1M -gpu 0 -preset slow -acodec aac -b:a 128k" \
   -e SAMBA_USER=testuser \
-  -e SAMBA_PASS=test123 \
+  -e SAMBA_PASS=test123  \
   -v /home/roger/zoomrec/recordings:/home/zoomrec/recordings \
   -v /home/roger/zoomrec/audio:/home/zoomrec/audio \
   -v /home/roger/zoomrec/meetings.csv:/home/zoomrec/meetings.csv:ro \
