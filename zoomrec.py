d049d92adc154d69c772afe3d346e9bae37a367c
Roger Kilchenmann <roger@kilchenmann.net>
2023-02-12 11:02:57 +1100

    rebuild schedule when meeting.csv changes

--- zoomrec.py
+++ zoomrec.py
@@ -914,6 +914,7 @@ def join_ongoing_meeting():
 
 
 def setup_schedule():
+    schedule.clear()
     with open(CSV_PATH, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=CSV_DELIMITER)
         line_count = 0
@@ -932,41 +933,6 @@ def setup_schedule():
                 line_count += 1
         logging.info("Added %s meetings to schedule." % line_count)
 
-# import telebot
-# from datetime import datetime
-# import threading
-
-# bot = telebot.TeleBot("YOUR_BOT_TOKEN")
-# scheduled_links = {}
-
-# def schedule_link(link, scheduled_time):
-#     while True:
-#         current_time = datetime.now()
-#         if current_time >= scheduled_time:
-#             bot.send_message(link["chat_id"], f"Event {link['name']} with link {link['link']} is now available!")
-#             scheduled_links.pop(link["chat_id"])
-#             break
-
-# def handle_schedule_command(message):
-#     parts = message.text.split()
-#     if len(parts) != 4:
-#         bot.reply_to(message, "Invalid command format. Use /schedule <event_name> <link> <day/time>")
-#     else:
-#         name = parts[1]
-#         link = parts[2]
-#         scheduled_time_str = parts[3]
-#         try:
-#             scheduled_time = datetime.strptime(scheduled_time_str, "%Y-%m-%d %H:%M:%S")
-#             if scheduled_time <= datetime.now():
-#                 bot.reply_to(message, "Scheduled time must be in the future.")
-#             else:
-#                 scheduled_links[message.chat.id] = {"name": name, "link": link, "time": scheduled_time, "chat_id": message.chat.id}
-#                 bot.reply_to(message, f"Event {name} with link {link} scheduled for {scheduled_time}")
-#                 threading.Thread(target=schedule_link, args=(scheduled_links[message.chat.id],)).start()
-#         except ValueError:
-#             bot.reply_to(message, "Invalid time format. Use YYYY-MM-DD HH:MM:SS")
-
-# bot.message_handler(commands=['schedule'])(handle_schedule_command)
 # threading.Thread(target=bot.polling, kwargs={"none_stop":True}).start()
 
 def main():
@@ -976,18 +942,20 @@ def main():
     except Exception:
         logging.error("Failed to create screenshot folder!")
         raise
-
-    setup_schedule()
-    join_ongoing_meeting()
-
+    last_timestamp = ''
+    while True:
+        current_timestamp = os.path.getmtime(CSV_PATH)
+        if current_timestamp != last_timestamp:
+            last_timestamp = current_timestamp
+            setup_schedule()
+            join_ongoing_meeting()
+    
+        schedule.run_pending()
+        time.sleep(1)
+        time_of_next_run = schedule.next_run()
+        time_now = datetime.now()
+        remaining = time_of_next_run - time_now
+        print(f"Next meeting in {remaining}", end="\r", flush=True)
 
 if __name__ == '__main__':
     main()
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-    time_of_next_run = schedule.next_run()
-    time_now = datetime.now()
-    remaining = time_of_next_run - time_now
-    print(f"Next meeting in {remaining}", end="\r", flush=True)
