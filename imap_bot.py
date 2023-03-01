c4116aecde4f5ad3f6a85d1e79413ccae364e1e2
Roger Kilchenmann <roger@kilchenmann.net>
2023-03-01 13:55:41 +1100

    fix email type 3 recognition

--- imap_bot.py
+++ imap_bot.py
@@ -45,98 +45,108 @@ def start_bot(CSV_PATH, IMAP_SERVER, IMAP_PORT, EMAIL_ADDRESS, EMAIL_PASSWORD):
         imap.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         imap.select('inbox')
 
-        # Get initial message count
-        status, count = imap.status('INBOX', "(MESSAGES UNSEEN)")
-        unread_count = int(count[0].split()[4].decode('utf-8').strip(').,]'))
+        # search for all unseen emails
+        #imap.search(None, 'UNSEEN')
 
-        # Get updated message count
-        status, count = imap.status('INBOX', "(MESSAGES UNSEEN)")
-        new_unread_count = int(count[0].split()[4].decode('utf-8').strip(').,]'))
+        for msg_id in imap.search(None, 'UNSEEN')[1][0].split():
+            status, msg_data = imap.fetch(msg_id, '(RFC822)')
 
-        # Check for new unread messages
-        if new_unread_count >= unread_count:
-            unread_count = new_unread_count
+            msg = email.message_from_bytes(msg_data[0][1])
 
-            # Process all unread messages
-            for i in range(unread_count, 0, -1):
-                status, msg_data = imap.fetch(str(i), '(RFC822)')
-                msg = email.message_from_bytes(msg_data[0][1])
+            # Check the subject line for the keyword
+            subject = msg['Subject']
+            if 'link' in subject:
 
-                # Check the subject line for the keyword
-                subject = msg['Subject']
-                if 'link' in subject:
+                url = ''
+                # Extract class information from the email body
+                for part in msg.walk():
+                    if part.get_content_type() == 'text/plain':
+                        body = part.get_payload(decode=True).decode('utf-8')
 
-                    # Extract class information from the email body
-                    for part in msg.walk():
-                        if part.get_content_type() == 'text/plain':
-                            body = part.get_payload(decode=True).decode('utf-8')
+                        # first type
+                        match = re.search(r'Your Human\.Kind Livestream - (.+) starts at ([0-9]+:[0-9]+ [AP]M) \((\S+)\)', body)
+                        if match:
+                            description = match.group(1)
+                            description = re.sub(r'[^a-zA-Z]+', '_', description)
 
-                            # first type
-                            match = re.search(r'Your Human\.Kind Livestream - (.+) starts at ([0-9]+:[0-9]+ [AP]M) \((\S+)\)', body)
+                            time_str = match.group(2)
+
+                            # time_system the input time to the system timezone
+                            time_system = parse_time( time_str, '%I:%M %p', datetime.now(), convert_short_tz(match.group(3)))
+                            time_system_str = time_system.strftime('%H:%M')
+                            date_system_str = time_system.strftime("%Y-%m-%d")
+
+                            url_pattern = r'<(https?://\S*loyal\.im\?\S*)>'
+                            match = re.search(url_pattern, body) 
+
                             if match:
-                                description = match.group(1)
-                                description = re.sub(r'[^a-zA-Z]+', '_', description)
+                                url = match.group(1)
+                        
+                        # second type
+                        # Search for the time and description using the pattern
+                        match = re.search(r'Your link to\s+(\d{1,2}(?:\.\d{2})?(?:am|pm))\s+([^\d]+)\swith\s+(.*)', subject, re.IGNORECASE)
+                        if match:
+                            # Extract the time and description from the match
+                            time_str = match.group(1)
+                            if '.' in time_str:
+                                time_pattern = '%I.%M%p'
+                            else:
+                                time_pattern = '%I%p'
 
-                                time_str = match.group(2)
+                            time_system = parse_time( time_str, time_pattern, datetime.now(), convert_short_tz('ACDT'))
+                            time_system_str = time_system.strftime('%H:%M')
+                            date_system_str = time_system.strftime("%Y-%m-%d")
 
-                                # time_system the input time to the system timezone
-                                time_system = parse_time( time_str, '%I:%M %p', datetime.now(), convert_short_tz(match.group(3)))
-                                time_system_str = time_system.strftime('%H:%M')
-                                date_system_str = time_system.strftime("%Y-%m-%d")
+                            description = match.group(2) + " " + match.group(3)
+                            description = re.sub(r'[^a-zA-Z]+', '_', description)
 
-                                url_pattern = r'<(https?://\S*loyal\.im\?\S*)>'
-                                match = re.search(url_pattern, body) 
+                            # Regex pattern to match zoom links
+                            ZOOM_LINK_REGEX = r"(https://.*zoom.us/\S*)"
 
-                                if match:
-                                    url = match.group(1)
-                            
-                            # second type
-                            # Search for the time and description using the pattern
-                            match = re.search(r'Your link to\s+(\d{1,2}(?:\.\d{2})?(?:am|pm))\s+([^\d]+)\swith\s+(.*)', subject, re.IGNORECASE)
+                            # search for the pattern in the string
+                            match = re.search(ZOOM_LINK_REGEX, body)
                             if match:
-                                # Extract the time and description from the match
-                                time_str = match.group(1)
-                                if '.' in time_str:
-                                    time_pattern = '%I.%M%p'
-                                else:
-                                    time_pattern = '%I%p'
+                                url = match.group()
 
-                                time_system = parse_time( time_str, time_pattern, datetime.now(), convert_short_tz('ACDT'))
-                                time_system_str = time_system.strftime('%H:%M')
-                                date_system_str = time_system.strftime("%Y-%m-%d")
+                        # third type
+                        match = re.search(r'([0-9]+:[0-9]+ [AP]M) \((\S+)\)', subject)
+                        if match:
+                            description = "Humankind_class"
 
-                                description = match.group(2) + " " + match.group(3)
-                                description = re.sub(r'[^a-zA-Z]+', '_', description)
+                            time_str = match.group(1)
 
-                                # Regex pattern to match zoom links
-                                ZOOM_LINK_REGEX = r"(https://.*zoom.us/\S*)"
+                            # time_system the input time to the system timezone
+                            time_system = parse_time( time_str, '%I:%M %p', datetime.now(), convert_short_tz(match.group(2)))
+                            time_system_str = time_system.strftime('%H:%M')
+                            date_system_str = time_system.strftime("%Y-%m-%d")
 
-                                # search for the pattern in the string
-                                match = re.search(ZOOM_LINK_REGEX, body)
-                                if match:
-                                    url = match.group()
+                            # Regex pattern to match zoom links
+                            ZOOM_LINK_REGEX = r"(https://.*zoom.us/\S*)"
 
-                            if url:
-                                event = {'description': description, 'weekday': date_system_str, 'time': time_system_str, 'duration': DURATION, 'id': url, 'password': '', 'record': RECORD}
-                                try:
-                                    event = validate_event( event)
-                                except ValueError as error:
-                                    print( error.args[0])
+                            # search for the pattern in the string
+                            match = re.search(ZOOM_LINK_REGEX, body)
+                            if match:
+                                url = match.group()
 
-                                events = read_events_from_csv(CSV_PATH)
-                                events.append(event)
-                                write_events_to_csv(CSV_PATH, events)
+                        if url:
+                            event = {'description': description, 'weekday': date_system_str, 'time': time_system_str, 'duration': DURATION, 'id': url, 'password': '', 'record': RECORD}
+                            try:
+                                event = validate_event( event)
+                            except ValueError as error:
+                                print( error.args[0])
 
-                                    
+                            events = read_events_from_csv(CSV_PATH)
+                            events.append(event)
+                            write_events_to_csv(CSV_PATH, events)
 
-                # Mark the message as read
-                imap.store(str(i), '+FLAGS', '\\Seen')
+                            # Mark the message as read
+                            imap.store(msg_id, '+FLAGS', '\\Seen')
 
         # Close the IMAP connection
         imap.close()
         imap.logout()   
 
-        # Wait for 5 seconds before checking again
+        # Wait for 5 mins before checking again
         time.sleep(5*60)
 
 if __name__ == "__main__":
