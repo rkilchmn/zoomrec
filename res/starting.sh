072fef6543843038412ec941c916336a97bd1559
Roger Kilchenmann <roger@kilchenmann.net>
2023-03-20 19:51:07 +1100

    change xfce.log location

--- res/starting.sh
+++ res/starting.sh
@@ -29,9 +29,7 @@ vncserver "$DISPLAY" -depth "$VNC_COL_DEPTH" -geometry "$VNC_RESOLUTION" &> "$ST
 echo -e "\nConnect to $VNC_IP:$VNC_PORT"
 
 # Start xfce4
-"$START_DIR"/xfce.sh &> "$START_DIR"/xfce.log
-
-sleep 1000000
+"$START_DIR"/xfce.sh &> /home/zoomrec/recordings/xfce.log
 
 # Cleanup to ensure pulseaudio is stateless
 rm -rf /var/run/pulse /var/lib/pulse /home/zoomrec/.config/pulse
