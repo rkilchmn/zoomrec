0928de2d604591ace25aa53b07e6ff22dc0ca8c8
Roger Kilchenmann <roger@kilchenmann.net>
2023-03-25 16:57:57 +1100

    touch

--- example/zoomrec_intel.sh
+++ example/zoomrec_intel.sh
@@ -8,6 +8,10 @@ docker run -d --restart unless-stopped --name zoomrec \
   -e LIBVA_DRIVER_NAME=i965 \
   -e SAMBA_USER=testuser \
   -e SAMBA_PASS=test123 \
+  -e IMAP_SERVER="imap.myemailprovider.com" \
+	-e IMAP_PORT="143"\
+	-e EMAIL_ADDRESS="zoomrec@myemailprovider.com" \
+	-e EMAIL_PASSWORD="mypassword" \
   -v /home/roger/zoomrec/recordings:/home/zoomrec/recordings \
   -v /home/roger/zoomrec/audio:/home/zoomrec/audio \
   -v /home/roger/zoomrec/meetings.csv:/home/zoomrec/meetings.csv \
