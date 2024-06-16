4c6591f44577480a3f8d918bd2ee9633518eb5ca
rkilchmn <roger@kilchenmann.net>
2024-06-16 16:29:54 +1000

    increase server timeout due to slow download resuilts in: [httpUpdate] Update.writeStream failed! (ERROR[6]: Stream Read Timeout)

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -4,6 +4,7 @@ import os
 bind = '0.0.0.0' +  ':' + os.getenv("DOCKER_API_PORT", "8080")
 worker_class = 'sync'
 loglevel = 'debug'
+timeout = 300  # Timeout set to 300 seconds, firmware download from ESP 8266 can be quite slow
 LOG_DIR = os.path.join(os.getenv('HOME'), os.getenv('LOG_SUBDIR'))
 accesslog = os.path.join(LOG_DIR,'gunicorn_access_log')
 acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
