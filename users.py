1abc5e810b02a8d6b06d2194a90a3e761f6a171d
rkilchmn <roger@kilchenmann.net>
2025-03-12 21:57:07 +1100

    remove logic for password change - API/Telegram Bot can ovvride password as they are running as super users

--- users.py
+++ users.py
@@ -21,9 +21,6 @@ class UserField(Enum):
     CREATED_TIMESTAMP = 'created_timestamp'
     LAST_UPDATED_TIMESTAMP = 'last_updated_timestamp'
 
-# required to update password
-PASSWORD_OLD = 'password_old'
-
 class UserRole:
     NORMAL = 1
     ADMIN = 2
@@ -97,10 +94,6 @@ class Users(ABC):
         for field in UserField:
             if field.value in user:
                 clean_user[field.value] = user[field.value]
-
-        # add PASSWORD_OLD
-        if PASSWORD_OLD in user:
-            clean_user[PASSWORD_OLD] = user[PASSWORD_OLD]
         return clean_user
     
     @staticmethod
@@ -243,20 +236,7 @@ class SQLLiteUser(Users):
 
     def update(self, user):
         user = Users.clean(user)
-        # password change requested
-        if UserField.PASSWORD.value in user:
-            # old password required and needs to authorize
-            existing_user = self.get(user[UserField.KEY.value])[0]
-            if PASSWORD_OLD in user and user[PASSWORD_OLD]:
-                if password.verify_password(user[PASSWORD_OLD], existing_user[UserField.PASSWORD.value]):
-                    user[UserField.PASSWORD.value] = password.hash_password(user[UserField.PASSWORD.value])
-                    # remove as it is not a database field
-                    del user[PASSWORD_OLD]
-                else:
-                    raise ValueError(f"Invalid '{PASSWORD_OLD}' for password update")
-            else:
-                raise ValueError(f"'{PASSWORD_OLD}' is required")
-
+        user[UserField.PASSWORD.value] = password.hash_password(user[UserField.PASSWORD.value])
         user[UserField.LAST_UPDATED_TIMESTAMP.value] = datetime.now()
         user = Users.validate(user)
         old_user = self.get(user[UserField.KEY.value])
