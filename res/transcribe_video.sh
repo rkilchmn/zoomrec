f227af2f8568940584066e9fbee781fe0f273b3b
rkilchmn <roger@kilchenmann.net>
2025-05-10 22:49:54 +1000

    quiet output

--- res/transcribe_video.sh
+++ res/transcribe_video.sh
@@ -25,9 +25,9 @@ audio_file="${input_file%.*}.mp3"
 
 # Extract audio mp3 using ffmpeg
 # ffmpeg -hide_banner -loglevel error -stats -y -i "$input_file" -vn -acodec copy "$audio_file"
-ffmpeg -hide_banner -loglevel error -stats -y -i "$input_file" -vn -acodec libmp3lame "$audio_file"
+ffmpeg -hide_banner -loglevel quiet -y -i "$input_file" -vn -acodec libmp3lame "$audio_file"
 
-whisper-ctranslate2-remote-api "$audio_file" --output_dir "$input_dir" --faster_whisper_api_base_url http://host.docker.internal:9876/api/v0
+whisper-ctranslate2-remote-api --verbose false "$audio_file" --output_dir "$input_dir" --faster_whisper_api_base_url http://host.docker.internal:9876/api/v0
 
 # Delete source audio file
 rm "$audio_file"
