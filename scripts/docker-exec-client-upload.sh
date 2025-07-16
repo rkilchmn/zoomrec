b87554b3c0d8f322e274076bab3cf4683b5d575f
rkilchmn <roger@kilchenmann.net>
2025-07-16 23:41:21 +1000

    fix variable substitution

--- scripts/docker-exec-client-upload.sh
+++ scripts/docker-exec-client-upload.sh
@@ -5,9 +5,10 @@ if [ "$#" -ne 3 ]; then
     echo "$usage"
     exit 1
 fi
+
 docker exec -u zoomrec zoomrec_client /home/zoomrec/client/sftp_upload.sh \
-  '/home/zoomrec/recordings/$1' \
-  '$2' \
-  '/home/zoomrec/.ssh/id_rsa' \
-  '$3/recordings' \
-  False
+  "/home/zoomrec/recordings/$1" \
+  "$2" \
+  "/home/zoomrec/.ssh/id_rsa" \
+  "$3/recordings" \
+  "False"
