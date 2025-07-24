7fef67ea8f8caa0e4748b2a630b39af3e70a2586
rkilchmn <roger@kilchenmann.net>
2025-07-24 21:28:02 +1000

    fix recording directory path

--- scripts/docker-exec-client-transcribe.sh
+++ scripts/docker-exec-client-transcribe.sh
@@ -6,4 +6,4 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-docker exec  -u zoomrec zoomrec_client /home/zoomrec/client/transcribe_video.sh transcribe /home/zoomrec/recordings/$1.mkv
+docker exec  -u zoomrec zoomrec_client /home/zoomrec/client/transcribe_video.sh transcribe /home/zoomrec/data/recordings/$1.mkv
