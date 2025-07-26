62f337e246b4f26148065d77d9a00f7ade596f61
rkilchmn <roger@kilchenmann.net>
2025-07-26 15:45:24 +1000

    adjusted samba server for data/recording dir and allow to change password per .env file

--- client/res/entrypoint.sh
+++ client/res/entrypoint.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 # executed with root
 echo -e "Starting Samba..."
+
+# Create Samba user and group
+if [ -n "$SAMBA_USERNAME" ]; then
+    echo "Creating Samba user $SAMBA_USERNAME and group samba..."
+    groupadd samba 2>/dev/null || true
+    id -u $SAMBA_USERNAME &>/dev/null || useradd $SAMBA_USERNAME
+    usermod -aG samba $SAMBA_USERNAME
+fi
+
+# Create Samba user and set password
+if [ -n "$SAMBA_PASSWORD" ]; then
+    echo -e "Creating Samba user $SAMBA_USERNAME..."
+    (echo "$SAMBA_PASSWORD"; echo "$SAMBA_PASSWORD") | smbpasswd -a -s $SAMBA_USERNAME
+fi
+
+# Set permissions for recordings directory
 setfacl -R -m "g:samba:rwx" ${HOME}/data/recordings
+
+# Start Samba services
 /usr/sbin/smbd -D  
 /usr/sbin/nmbd -D 
 
@@ -9,6 +27,6 @@ setfacl -R -m "g:samba:rwx" ${HOME}/data/recordings
 # chown -R zoomrec:zoomrec ${HOME}/data
 # chown -R zoomrec:zoomrec ${HOME}/config
 
-# start application witwith non-root
+# start application with non-root
 echo -e "Starting Zoomrec..."
 exec su zoomrec -p -c "${START_DIR}/starting.sh"
