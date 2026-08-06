42c461776250f84692fe324e70c151aa4960608d
rkilchmn <roger@kilchenmann.net>
2026-08-06 18:51:33 +1000

    Security Fix: add basic auth requirement to update_event endpoint

--- server/zoomrec_server_app.py
+++ server/zoomrec_server_app.py
@@ -717,6 +717,7 @@ def create_event():
 #     }' \
 #     "http://localhost:8081/event/G4JbZYQN65Ba35jfbyiHsj"
 @app.route(f"{constants.ROUTE_EVENT}/<key>", methods=["PUT"])
+@basic_auth.required
 def update_event(key):
     try:
         event = request.json
@@ -1172,7 +1173,7 @@ def _range_response(path, mimetype='video/mp4'):
         return Response('Internal Server Error', status=500, headers=headers)
 
 # List files for a resource
-@app.route(f"{constants.ROUTE_LIST}/<access_key>/<path:resource>")
+@app.route(f"{constants.ROUTE_LIST}/<access_key>/<path:resource}")
 def list_files(access_key, resource):
     """
     List all files for a resource that the access key has access to.
