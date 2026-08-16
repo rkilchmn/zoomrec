d96b59622f52d9862cfe448b6be09d8107ee23e5
rkilchmn <roger@kilchenmann.net>
2026-08-16 13:52:36 +1000

    allow VAAPI alternate render node (with default)

---
+++ client/res/test_vaapi_x264.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# for vaapi under wls2 with intel xe integrated gpu
+vainfo --display drm
+ffmpeg -hwaccel vaapi -hwaccel_output_format vaapi -init_hw_device vaapi=foo:/dev/dri/$LIBVA_RENDER_NODE -i ~/recordings/test.mkv -map 0:v:0 -c:v h264_vaapi ~/recordings/test_vaapi_h264.mkv
